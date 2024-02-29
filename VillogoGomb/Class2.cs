32e561cf6adddf3a82386140f91a9a489418c81a
rttjnk <janka.rott@stud.uni-corvinus.hu>
2024-02-29 13:04:04 +0100

    3.gyakorlat

---
+++ VillogoGomb/Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillogoGomb
+{
+    internal class Szinezogomb : Button
+    {
+        public Szinezogomb()
+        {
+            Height=20;
+            Width=20;
+            
+            MouseClick += Szinezogomb_MouseClick;
+        }
+
+        private void Szinezogomb_MouseClick(object? sender, MouseEventArgs e)
+        {
+            if (BackColor != Color.Fuchsia)
+            {
+                BackColor = Color.Fuchsia;
+            }
+            else { BackColor = SystemColors.ButtonFace; }
+        }
+    }
+}
---
+++ VillogoGomb/Class3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillogoGomb
+{
+    internal class Szamologomb:Button
+    {
+        int szám;
+        
+        public Szamologomb()
+        {
+            Height = 20;
+            Width = 20;
+            szám = 0;
+            MouseClick += Szamologomb_MouseClick;
+            
+        }
+
+        private void Szamologomb_MouseClick(object? sender, MouseEventArgs e)
+        {
+            szám++;
+            Text = szám.ToString();
+            if (szám == 5)
+            {
+                szám = 0;
+            }
+        }
+    }
+}
