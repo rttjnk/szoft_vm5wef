a6b7701c0844eed0bbd40ffc088f7e48ae87c394
rttjnk <janka.rott@stud.uni-corvinus.hu>
2024-03-14 13:05:24 +0100

    kész órai

--- HajosTeszt/HajosTeszt/Form1.Designer.cs
+++ HajosTeszt/HajosTeszt/Form1.Designer.cs
@@ -29,7 +29,10 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
@@ -42,21 +45,44 @@
             dataGridView1.Size = new Size(803, 124);
             dataGridView1.TabIndex = 0;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(430, 80);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(325, 240);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
             Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dataGridView1;
+        private Label label1;
+        private PictureBox pictureBox1;
     }
 }
--- HajosTeszt/HajosTeszt/Form1.cs
+++ HajosTeszt/HajosTeszt/Form1.cs
@@ -6,19 +6,35 @@ namespace HajosTeszt
 
         List<Kérdés> AktuálisKérdések;
 
+        VálaszGomb VálaszGomb1;
+        VálaszGomb VálaszGomb2;
+        VálaszGomb VálaszGomb3;
+
         int Aktuáliskérdés = 5;
 
 
         public Form1()
         {
             InitializeComponent();
+            VálaszGomb1 = new VálaszGomb();
+            VálaszGomb1.Top = 50;
+            Controls.Add(VálaszGomb1);
+
+            VálaszGomb2 = new VálaszGomb();
+            VálaszGomb2.Top = 150;
+            Controls.Add(VálaszGomb2);
+
+            VálaszGomb3 = new VálaszGomb();
+            VálaszGomb3.Top = 250;
+            Controls.Add(VálaszGomb3);
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             ÖsszesKérdés = KérdésBetöltés();
             AktuálisKérdések = new List<Kérdés>();
-            
+
             for (int i = 0; i < 7; i++)
             {
                 AktuálisKérdések.Add(ÖsszesKérdés[0]);
@@ -26,6 +42,26 @@ namespace HajosTeszt
             }
             dataGridView1.DataSource = AktuálisKérdések;
 
+            Kérdésmegjelenítés(AktuálisKérdések[Aktuáliskérdés]);
+
+        }
+
+        void Kérdésmegjelenítés(Kérdés kérdés)
+        {
+            label1.Text = kérdés.KérdésSzöveg;
+            VálaszGomb1.Text = kérdés.Válasz1;
+            VálaszGomb2.Text = kérdés.Válasz2;
+            VálaszGomb3.Text = kérdés.Válasz3;
+
+            if (string.IsNullOrEmpty(kérdés.URL))
+            {
+                pictureBox1.Visible = false;
+            }
+            else
+            {
+                pictureBox1.Visible = true;
+                pictureBox1.Load("https://storage.altinum.hu/hajo/" + kérdés.URL);
+            }
         }
 
         List<Kérdés> KérdésBetöltés()
